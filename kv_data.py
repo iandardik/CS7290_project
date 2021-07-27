c465ec71a893034591b41c8edec088801925925e
iandardik <iandardik1@gmail.com>
2021-07-27 18:42:19 -0400

    adding pts back in

--- kv_data.py
+++ kv_data.py
@@ -7,6 +7,7 @@ tdsNextYear = []
 fumblesNextYear = []
 ygNextYear = []
 gamesNextYear = []
+ptsNextYear = []
 
 for index, row in data.iterrows():
     player = row["ID"]
@@ -17,6 +18,7 @@ for index, row in data.iterrows():
     fumbles = "N/A"
     yg = "N/A"
     games = "N/A"
+    points = "N/A"
     if next_year.shape[0]:
         assert(next_year.shape[0] == 1)
         yards = next_year["Yards"].to_numpy()[0]
@@ -24,15 +26,18 @@ for index, row in data.iterrows():
         fumbles = next_year["Fumbles"].to_numpy()[0]
         yg = next_year["Y/G"].to_numpy()[0]
         games = next_year["Games"].to_numpy()[0]
+        points = 0.1*yards + 6.0*tds - 2.0*fumbles
     yardsNextYear.append(yards)
     tdsNextYear.append(tds)
     fumblesNextYear.append(fumbles)
     ygNextYear.append(yg)
     gamesNextYear.append(games)
+    ptsNextYear.append(points)
 
 data["Yards Next Year"] = yardsNextYear
 data["TD Next Year"] = tdsNextYear
 data["Fumbles Next Year"] = fumblesNextYear
 data["Y/G Next Year"] = ygNextYear
 data["Games Next Year"] = gamesNextYear
+data["Points Next Year"] = ptsNextYear
 data.to_csv("data.csv", index=False, float_format="{:.4f}".format)
