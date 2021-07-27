c2f1157981eb661d7e9627357e8833be775c5100
iandardik <iandardik1@gmail.com>
2021-07-27 17:36:50 -0400

    adding Y/G

--- kv_data.py
+++ kv_data.py
@@ -5,6 +5,7 @@ data = pandas.read_csv("raw_data.csv")
 yardsNextYear = []
 tdsNextYear = []
 fumblesNextYear = []
+ygNextYear = []
 
 for index, row in data.iterrows():
     player = row["ID"]
@@ -13,16 +14,20 @@ for index, row in data.iterrows():
     yards = "N/A"
     tds = "N/A"
     fumbles = "N/A"
+    yg = "N/A"
     if next_year.shape[0]:
         assert(next_year.shape[0] == 1)
         yards = next_year["Yards"].to_numpy()[0]
         tds = next_year["TD"].to_numpy()[0]
         fumbles = next_year["Fumbles"].to_numpy()[0]
+        yg = next_year["Y/G"].to_numpy()[0]
     yardsNextYear.append(yards)
     tdsNextYear.append(tds)
     fumblesNextYear.append(fumbles)
+    ygNextYear.append(yg)
 
 data["Yards Next Year"] = yardsNextYear
 data["TD Next Year"] = tdsNextYear
 data["Fumbles Next Year"] = fumblesNextYear
+data["Y/G Next Year"] = ygNextYear
 data.to_csv("data.csv", index=False, float_format="{:.4f}".format)
