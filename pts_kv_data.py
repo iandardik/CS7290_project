3279a9e49713abb89a8ea61c9230fe434863e178
iandardik <iandardik1@gmail.com>
2021-07-27 17:33:23 -0400

    adding next years stats individually

--- pts_kv_data.py
+++ pts_kv_data.py
@@ -2,27 +2,20 @@ import pandas
 import numpy as np
 
 data = pandas.read_csv("raw_data.csv")
-yardsNextYear = []
-tdsNextYear = []
-fumblesNextYear = []
+ptsNextYear = []
 
 for index, row in data.iterrows():
     player = row["ID"]
     year = row["Year"]
     next_year = data.loc[(data["ID"] == player) & (data["Year"] == year+1)]
-    yards = "N/A"
-    tds = "N/A"
-    fumbles = "N/A"
+    points = "N/A"
     if next_year.shape[0]:
         assert(next_year.shape[0] == 1)
         yards = next_year["Yards"].to_numpy()[0]
         tds = next_year["TD"].to_numpy()[0]
         fumbles = next_year["Fumbles"].to_numpy()[0]
-    yardsNextYear.append(yards)
-    tdsNextYear.append(tds)
-    fumblesNextYear.append(fumbles)
+        points = 0.1*yards + 6.0*tds - 2.0*fumbles
+    ptsNextYear.append(points)
 
-data["Yards Next Year"] = yardsNextYear
-data["TD Next Year"] = tdsNextYear
-data["Fumbles Next Year"] = fumblesNextYear
+data["Points Next Year"] = ptsNextYear
 data.to_csv("data.csv", index=False, float_format="{:.4f}".format)
