f47a6892986c3114d5eea9333579ac4247e04e0c
iandardik <iandardik1@gmail.com>
2021-07-27 17:38:41 -0400

    adding Games

--- kv_data.py
+++ kv_data.py
@@ -6,6 +6,7 @@ yardsNextYear = []
 tdsNextYear = []
 fumblesNextYear = []
 ygNextYear = []
+gamesNextYear = []
 
 for index, row in data.iterrows():
     player = row["ID"]
@@ -15,19 +16,23 @@ for index, row in data.iterrows():
     tds = "N/A"
     fumbles = "N/A"
     yg = "N/A"
+    games = "N/A"
     if next_year.shape[0]:
         assert(next_year.shape[0] == 1)
         yards = next_year["Yards"].to_numpy()[0]
         tds = next_year["TD"].to_numpy()[0]
         fumbles = next_year["Fumbles"].to_numpy()[0]
         yg = next_year["Y/G"].to_numpy()[0]
+        games = next_year["Games"].to_numpy()[0]
     yardsNextYear.append(yards)
     tdsNextYear.append(tds)
     fumblesNextYear.append(fumbles)
     ygNextYear.append(yg)
+    gamesNextYear.append(games)
 
 data["Yards Next Year"] = yardsNextYear
 data["TD Next Year"] = tdsNextYear
 data["Fumbles Next Year"] = fumblesNextYear
 data["Y/G Next Year"] = ygNextYear
+data["Games Next Year"] = gamesNextYear
 data.to_csv("data.csv", index=False, float_format="{:.4f}".format)
